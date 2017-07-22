1ce289ed684352b17aa93fe6b73838c874dc8f32
anumagar <anumagar>
2017-07-22 09:08:38 +0530

    Udhay changes

--- js/viewModels/header.js
+++ js/viewModels/header.js
@@ -7,131 +7,131 @@
  */
 define(['knockout', 'ojs/ojcore', 'ojs/ojknockout', 'ojs/ojnavigationlist', 'ojs/ojoffcanvas', 'ojs/ojdatacollection-common', 'ojs/ojdialog'
 ], function (ko, oj) {
-    /**
-     * The view model for the header module
-     */
+  /**
+   * The view model for the header module
+   */
 
-    function HeaderViewModel() {
-        var self = this;
+  function HeaderViewModel() {
+    var self = this;
 
-        //
-        // Button used for toggling off screen data.
-        //
-        var offScreenDataButton = {
-            "label": "offscreen-toggle",
-            "iconClass": "oj-web-applayout-offcanvas-icon",
-            "url": "#"
-        };
+    //
+    // Button used for toggling off screen data.
+    //
+    var offScreenDataButton = {
+      "label": "offscreen-toggle",
+      "iconClass": "oj-web-applayout-offcanvas-icon",
+      "url": "#"
+    };
 
-        //
-        // Dropdown menu states
-        //
+    //
+    // Dropdown menu states
+    //
 
-        self.menuItemSelect = function (event, ui) {
-            switch (ui.item.attr("id")) {
-                case "About":
-                    $("#aboutDialog").ojDialog("open");
-                    break;
-                default:
-            }
-        };
+    self.menuItemSelect = function (event, ui) {
+      switch (ui.item.attr("id")) {
+        case "About":
+          $("#aboutDialog").ojDialog("open");
+          break;
+        default:
+      }
+    };
 
-        // Data for application name
-        var appName = {
-            "id": "qs",
-            "name": "Oracle Universal Dashboard"
-        };
+    // Data for application name
+    var appName = {
+      "id": "qs",
+      "name": "Oracle Universal Dashboard"
+    };
 
-        //
-        // Toolbar buttons
-        //
-        var toolbarData = {
-            // user name in toolbar
-            "userName": "kshitiz.anand@oracle.com",
-            "toolbar_buttons": [
-                {
-                    "label": "toolbar_search_button",
-                    "iconClass": "oj-fwk-icon-magnifier oj-fwk-icon",
-                    "url": "#"
-                }
-            ],
-            // Data for global nav dropdown menu embedded in toolbar.
-            "global_nav_dropdown_items": [
-                {"label": "Preferences",
-                    "url": "#"
-                },
-                {"label": "Help",
-                    "url": "#"
-                },
-                {"label": "Sign Out",
-                    "url": "#"
-                },
-                {"label": "About",
-                    "url": "#"
-                }
-            ]
-        };
-
-        self.offScreenButtonIconClass = offScreenDataButton.iconClass;
-        self.offScreenButtonLabel = offScreenDataButton.label;
-
-        self.appId = appName.id;
-        self.appName = appName.name;
-
-        self.userName = ko.observable(toolbarData.userName);
-        self.toolbarButtons = toolbarData.toolbar_buttons;
-        self.globalNavItems = toolbarData.global_nav_dropdown_items;
-
-        self.appDrawer =
-                {
-                    "edge": "start",
-                    "displayMode": "push",
-                    "selector": "#appDrawer",
-                    "selection": "selectedItem",
-                    "content": '#pageContent'
-                };
-
-        //
-        // Data for application navigation
-        //
-        var router = oj.Router.rootInstance;
-        var appNavData = [
-            {
-                name: router.states[0].label,
-                id: router.states[0].id,
-                disabled: 'false',
-                iconClass: 'demo-navi-dashboard-icon oj-navigationlist-item-icon'
-            },
-            {
-                name: router.states[1].label,
-                id: router.states[1].id,
-                disabled: 'false',
-                iconClass: 'demo-navi-person-icon oj-navigationlist-item-icon'
-            },
-            {
-                name: router.states[2].label,
-                id: router.states[2].id,
-                disabled: 'true',
-                iconClass: 'oj-disabled demo-navi-people-icon oj-navigationlist-item-icon'
-            }];
-
-        self.dataSource = new oj.ArrayTableDataSource(appNavData, {idAttribute: 'id'});
-
-        self.toggleAppDrawer = function ()
+    //
+    // Toolbar buttons
+    //
+    var toolbarData = {
+      // user name in toolbar
+      "userName": "kshitiz.anand@oracle.com",
+      "toolbar_buttons": [
         {
-            return oj.OffcanvasUtils.toggle(self.appDrawer);
-        };
+          "label": "toolbar_search_button",
+          "iconClass": "oj-fwk-icon-magnifier oj-fwk-icon",
+          "url": "#"
+        }
+      ],
+      // Data for global nav dropdown menu embedded in toolbar.
+      "global_nav_dropdown_items": [
+        {"label": "Preferences",
+          "url": "#"
+        },
+        {"label": "Help",
+          "url": "#"
+        },
+        {"label": "Sign Out",
+          "url": "#"
+        },
+        {"label": "About",
+          "url": "#"
+        }
+      ]
+    };
 
-        //
-        // Close off-screen content once window becomes larger.
-        //
-        var query = window.matchMedia("(min-width: 39.375rem)");
-        var mqListener = function (event)
-        {
-            oj.OffcanvasUtils.close(self.appDrawer);
-        };
-        query.addListener(mqListener);
+    self.offScreenButtonIconClass = offScreenDataButton.iconClass;
+    self.offScreenButtonLabel = offScreenDataButton.label;
 
-    }
-    return HeaderViewModel;
+    self.appId = appName.id;
+    self.appName = appName.name;
+
+    self.userName = ko.observable(toolbarData.userName);
+    self.toolbarButtons = toolbarData.toolbar_buttons;
+    self.globalNavItems = toolbarData.global_nav_dropdown_items;
+
+    self.appDrawer =
+            {
+              "edge": "start",
+              "displayMode": "push",
+              "selector": "#appDrawer",
+              "selection": "selectedItem",
+              "content": '#pageContent'
+            };
+
+    //
+    // Data for application navigation
+    //
+    var router = oj.Router.rootInstance;
+    var appNavData = [
+      {
+        name: router.states[0].label,
+        id: router.states[0].id,
+        disabled: 'false',
+        iconClass: 'demo-navi-dashboard-icon oj-navigationlist-item-icon'
+      },
+      {
+        name: router.states[1].label,
+        id: router.states[1].id,
+        disabled: 'false',
+        iconClass: 'demo-navi-person-icon oj-navigationlist-item-icon'
+      },
+      {
+        name: router.states[2].label,
+        id: router.states[2].id,
+        disabled: 'false',
+        iconClass: ' demo-navi-people-icon oj-navigationlist-item-icon'
+      }];
+
+    self.dataSource = new oj.ArrayTableDataSource(appNavData, {idAttribute: 'id'});
+
+    self.toggleAppDrawer = function ()
+    {
+      return oj.OffcanvasUtils.toggle(self.appDrawer);
+    };
+
+    //
+    // Close off-screen content once window becomes larger.
+    //
+    var query = window.matchMedia("(min-width: 39.375rem)");
+    var mqListener = function (event)
+    {
+      oj.OffcanvasUtils.close(self.appDrawer);
+    };
+    query.addListener(mqListener);
+
+  }
+  return HeaderViewModel;
 });
